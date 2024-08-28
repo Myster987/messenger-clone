7b286c334991550a0731cfe613c53cd161059611
Mikołaj Maciejak <myster123987@gmail.com>
2024-08-28 21:13:20 +0200

    Frontend: new component to navigate to home page

---
+++ apps/frontend/src/lib/components/custom/buttons/BackToLinkButton.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { cn } from '@/utils';
+	import { ArrowLeft } from 'lucide-svelte';
+	import { Button } from '@/components/ui/button';
+
+	export let href: string;
+
+	let className: string | undefined = undefined;
+	export { className as class };
+</script>
+
+<a {href}>
+	<Button variant="ghost" size="icon" class={cn('rounded-full', className)}><ArrowLeft /></Button>
+</a>
--- apps/frontend/src/lib/components/custom/buttons/index.ts
+++ apps/frontend/src/lib/components/custom/buttons/index.ts
@@ -1,4 +1,5 @@
 export { default as ThemeButton } from './ThemeButton.svelte';
 export { default as SignOutButton } from './SignOutButton.svelte';
 export { default as LeaveGroupButton } from './LeaveGroupButton.svelte';
+export { default as BackToLinkButton } from './BackToLinkButton.svelte';
 export { default as UserProfileButton } from './UserProfileButton.svelte';
