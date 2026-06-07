19ed69fb96c5f8b1c0d8f4d23ee1940fdaa63562
Mikołaj Maciejak <myster123987@gmail.com>
2026-06-07 13:30:47 +0200

    fix: frontend - disable noExternal and just copy nod_modules from builder

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -2,8 +2,8 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()],
-	ssr: {
-		noExternal: true
-	}
+	plugins: [sveltekit()]
+	// ssr: {
+	// 	noExternal: true
+	// }
 });
