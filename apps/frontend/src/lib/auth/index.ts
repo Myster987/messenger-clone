db8144c66db1d209ee6ab47775eb88d7fda14f00
Mikołaj Maciejak <myster123987@gmail.com>
2026-06-07 14:07:43 +0200

    refactor: frontend - disable better-auth logger

--- apps/frontend/src/lib/auth/index.ts
+++ apps/frontend/src/lib/auth/index.ts
@@ -44,6 +44,13 @@ export const auth = betterAuth({
 	baseURL: env.BETTER_AUTH_URL || 'http://placeholder',
 	secret: env.BETTER_AUTH_SECRET || 'dummysecret123',
 
+	telemetry: {
+		enabled: false
+	},
+	logger: {
+		disabled: true
+	},
+
 	plugins: [sveltekitCookies(getRequestEvent)]
 });
 
